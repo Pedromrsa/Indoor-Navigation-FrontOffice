0ab31af396975f6f1cbe50724bfb4644388f22ca
DiogoFerreiraGoncalves <1211509@isep.ipp.pt>
2025-05-02 23:13:34 +0100

    Make the login page Work and Update Path Problems UI

--- App/IndoorMappingApp/LoginPage.xaml.cs
+++ App/IndoorMappingApp/LoginPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace IndoorMappingApp
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace IndoorMappingApp
 {
     public partial class LoginPage : ContentPage
     {
@@ -8,7 +12,62 @@
         }
         async void OnRegisterClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new RegisterPage());
+            await Navigation.PushAsync(new RegisterPage(),true);
+        }
+
+        private async void OnForgotPasswordClicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new RecoverAccountPage());
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            string email = emailEntry.Text;
+            string password = passwordEntry.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                DisplayAlert("Login Error", "Please enter both email and password.", "OK");
+                return;
+            }
+            else
+            {
+
+                var client = new HttpClient();
+                var url = "https://isepindoornavigationapi-vgq7.onrender.com/api/Auth/login";
+
+
+                var requestData = new
+                {
+                    email = email,
+                    password = password
+                };
+
+                var json = JsonSerializer.Serialize(requestData);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        await Shell.Current.GoToAsync("mainmenu", true, new Dictionary<string, object>
+                        {
+                            { "IsGuest", false }
+                        });
+                    }
+                    else
+                    {
+                        await DisplayAlert("Login Error", $"Invalid Credentials", "OK");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Erro", $"Exceção: {ex.Message}", "OK");
+                }
+            }
         }
     }
 }
